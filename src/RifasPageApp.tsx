1a8b93047c35d20456a33bf3d3200ad08242a920
Diego Rodríguez <darh90@gmail.com>
2025-12-04 02:08:02 -0500

    el snoefall se dejo en un componente meno

--- src/RifasPageApp.tsx
+++ src/RifasPageApp.tsx
@@ -1,25 +1,9 @@
 import { RouterProvider } from "react-router"
 import { appRouter } from "./app.router"
-import Snowfall from 'react-snowfall'
+
 
 export const RifasPageApp = () => {
   return (
-    <>
-      <Snowfall 
-        color="#b0c4de"
-        snowflakeCount={150}
-        speed={[0.5, 2]}
-        wind={[-0.5, 1]}
-        radius={[0.5, 2]}
-        style={{
-          position: 'fixed',
-          width: '100vw',
-          height: '100vh',
-          zIndex: 9999,
-          pointerEvents: 'none',
-        }}
-      />
-      <RouterProvider router={appRouter}/>
-    </>
+    <RouterProvider router={appRouter}/>
   )
 }
--- src/single_page/layouts/SinglePageLayout.tsx
+++ src/single_page/layouts/SinglePageLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router"
 import { CustomHeader } from "../components/CustomHeader"
 import { CustomFooter } from "../components/CustomFooter"
+import Snowfall from "react-snowfall"
 
 
 
@@ -8,6 +9,20 @@ import { CustomFooter } from "../components/CustomFooter"
 export const SinglePageLayout = () => {
   return (
      <div className="min-h-screen bg-background">
+      <Snowfall 
+              color="#b0c4de"
+              snowflakeCount={150}
+              speed={[0.5, 2]}
+              wind={[-0.5, 1]}
+              radius={[0.5, 2]}
+              style={{
+                position: 'fixed',
+                width: '100vw',
+                height: '100vh',
+                zIndex: 9999,
+                pointerEvents: 'none',
+              }}
+            />
       <CustomHeader/>
       <Outlet/>
       <CustomFooter/>
